fae53ff994db7ac9267db15f8e06d201b0baa5bf
valyndsilva <valyndsilva@gmail.com>
2022-12-27 13:35:35 +0000

    clean up top communities

--- app/TopCommunities.tsx
+++ app/TopCommunities.tsx
@@ -14,22 +14,21 @@ function TopCommunities({}: Props) {
   });
   console.log(data);
   const subreddits: Subreddit[] = data?.getSubredditListLimit;
-  {
-    subreddits && (
-      <div className="flex-col sticky top-36 ml-5 mt-5 hidden h-fit min-w-[300px] rounded-md border border-gray-300 bg-white dark:bg-gray-700 lg:inline-flex">
-        <p className="text-md mb-1 p-4 pb-3 font-bold">Top Communities</p>
-        <div>
-          {subreddits?.map((subreddit, index) => (
-            <SubredditRow
-              key={subreddit.id}
-              topic={subreddit.topic}
-              index={index}
-            />
-          ))}
-        </div>
+
+  return (
+    <div className="flex-col sticky top-36 ml-5 mt-5 hidden h-fit min-w-[300px] rounded-md border border-gray-300 bg-white dark:bg-gray-700 lg:inline-flex">
+      <p className="text-md mb-1 p-4 pb-3 font-bold">Top Communities</p>
+      <div>
+        {subreddits?.map((subreddit, index) => (
+          <SubredditRow
+            key={subreddit.id}
+            topic={subreddit.topic}
+            index={index}
+          />
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default TopCommunities;
