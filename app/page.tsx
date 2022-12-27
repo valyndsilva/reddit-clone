febb68396547a66e6f9917c6f9aaf930382cde65
valyndsilva <valyndsilva@gmail.com>
2022-12-27 13:37:49 +0000

    Top communities on session

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
+import { unstable_getServerSession } from "next-auth";
 import React from "react";
 import Feed from "./Feed";
 import PostBox from "./PostBox";
 import TopCommunities from "./TopCommunities";
 type Props = {};
 
-function Home({}: Props) {
+async function Home({}: Props) {
+  const session = await unstable_getServerSession();
   return (
     <div className="my-7 mx-auto max-w-5xl">
       <PostBox />
       <div className="flex">
         <Feed />
         {/* List subreddits */}
-        <TopCommunities />
+        {session && <TopCommunities />}
       </div>
     </div>
   );
